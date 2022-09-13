5bc31f2eb30337a3ec8b5a5942bcfb38d9fa664d
Konr Ness <konrness@gmail.com>
2022-09-13 13:15:08 -0500

    Using httpbin for test endpoint

--- testrail/test.sh
+++ testrail/test.sh
@@ -1,6 +1,6 @@
 trcli --help
 
-export TESTRAIL_URL=https://example.testrail.com/
+export TESTRAIL_URL=https://httpbin.org/anything/
 export TESTRAIL_EMAIL=example@mail.com
 export TESTRAIL_PASSWORD=password
 

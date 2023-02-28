f9206a42759454a434bec00c7dac8d2e86973478
Konr Ness <konrness@gmail.com>
2023-02-28 15:46:52 -0600

    Added support for libraries, and CPEs Added error handling for empty/missing result types

--- wiz-to-harness/wizToHarness.js
+++ wiz-to-harness/wizToHarness.js
@@ -23,25 +23,104 @@ if (!outputFile || outputFile == "") {
 
 let fileContents = fs.readFileSync(inputFile).toString();
 let matches = fileContents.match(/({"[\s\S]*})/);
-let data = JSON.parse(matches[0]);
-let flattened = data.result.osPackages.flatMap((item) => {
-    return item.vulnerabilities.map((v) => ({
-        packageName: item.name,
-        version: item.version,
-        issueName: v.name,
-        issueDescription: "",
-        scanTool: "Wiz",
-        scanSeverity: v.severity,
-        severity: SEVMAP[v.severity] ? SEVMAP[v.severity] : 0,
-        remediationSteps: `Fixed version: ${v.fixedVersion}`,
-        url: v.source
-    }));
-});
+
+// check if valid JSON
+try {
+    if (null === matches) {
+        console.log(matches);
+        throw "No JSON objects found.";
+    }
+
+    var data = JSON.parse(matches[0]);
+
+    // validate proper format
+    if (typeof data.result !== 'object') {
+        throw "No data.result found in JSON";
+    }
+} catch (err) {
+    console.error("Error parsing Input JSON file.", err);
+    process.exit(-1);
+}
+
+let flattened = [];
+
+// Parse osPackages
+if (null != data.result.osPackages && data.result.osPackages.length > 0) {
+    console.debug("Parsing osPackages");
+
+    let osPackages = data.result.osPackages.flatMap((item) => {
+        return item.vulnerabilities.map((v) => ({
+            packageName: item.name,
+            version: item.version,
+            issueName: v.name,
+            issueDescription: "osPackage",
+            scanTool: "Wiz",
+            scanSeverity: v.severity,
+            severity: SEVMAP[v.severity] ? SEVMAP[v.severity] : 0,
+            remediationSteps: `Fixed version: ${v.fixedVersion}`,
+            url: v.source
+        }));
+    });
+
+    console.debug(" - Found ", osPackages.length);
+    
+    flattened = flattened.concat(osPackages);
+}
+
+// Parse libraries
+if (null != data.result.libraries && data.result.libraries.length > 0) {
+    console.debug("Parsing libraries");
+
+    let cpes = data.result.libraries.flatMap((item) => {
+        return item.vulnerabilities.map((v) => ({
+            packageName: item.name,
+            version: item.version,
+            issueName: v.name,
+            issueDescription: "library",
+            fileName: item.path,
+            scanTool: "Wiz",
+            scanSeverity: v.severity,
+            severity: SEVMAP[v.severity] ? SEVMAP[v.severity] : 0,
+            remediationSteps: `Fixed version: ${v.fixedVersion}`,
+            url: v.source
+        }));
+    });
+
+    console.debug(" - Found ", cpes.length);
+
+    flattened = flattened.concat(cpes);
+}
+
+// Parse CPEs
+if (null != data.result.cpes && data.result.cpes.length > 0) {
+    console.debug("Parsing CPEs");
+
+    let libraries = data.result.cpes.flatMap((item) => {
+        return item.vulnerabilities.map((v) => ({
+            packageName: item.name,
+            version: item.version,
+            issueName: v.name,
+            issueDescription: "CPE",
+            fileName: item.path,
+            scanTool: "Wiz",
+            scanSeverity: v.severity,
+            severity: SEVMAP[v.severity] ? SEVMAP[v.severity] : 0,
+            remediationSteps: `Fixed version: ${v.fixedVersion}`,
+            url: v.source
+        }));
+    });
+
+    console.debug(" - Found ", libraries.length);
+
+    flattened = flattened.concat(libraries);
+}
+
+console.debug("Total count: ", flattened.length);
 
 let result = {
     meta: {
         key: [
-            "issueName"
+            "packageName"
         ]
     },
     issues: flattened

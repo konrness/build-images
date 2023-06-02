8584b72cd5e4b443c088126906576bff8da119e7
Konr Ness <konrness@gmail.com>
2023-06-02 15:59:23 -0500

    Added DRONE_OUTPUT to debug

--- debug/README.md
+++ debug/README.md
@@ -1,2 +1,9 @@
 # Debug Drone/Harness Plugin
 CI plugin to debug Drone Plugin inputs and environment variables
+
+
+# Commands to Build
+```docker build -t debug .```
+
+# Commands to Run
+```docker run --rm --env-file ./.env -v $(pwd):/tmp debug```
--- debug/entrypoint.sh
+++ debug/entrypoint.sh
@@ -6,5 +6,10 @@ echo "-- Printing all Environment Variables..."
 
 printenv
 
+echo "-- Updating DRONE_OUTPUT .env file..."
+
+touch $DRONE_OUTPUT
+echo "foo=bar" >> $DRONE_OUTPUT
+
 endtime=$(date +%s.%N)
 echo "duration: $(echo "$endtime $starttime" | awk '{printf "%f", $1 - $2}')s"
